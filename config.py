947e64f9b4f71d061b3733b3b6569a6236e969ed
camege <48480492+camege@users.noreply.github.com>
2021-09-27 14:09:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-CLIENT_KEY = "test_7ZCG2HRVQ5DYDG54CTVFVXKYFIKWXTBF"
+CLIENT_KEY = ""
 HEADERS = {
-    "x-api-key": "AQEyhmfxKonIYxZGw0m/n3Q5qf3VaY9UCJ14XWZE03G/k2NFikzVGEiYj+4vtN01BchqAcwQwV1bDb7kfNy1WIxIIkxgBw==-JtQ5H0iXtu8rqQMD6iAb33gf2qZeGKGhrMpyQAt9zsw=-3wAkV)*$kP%bCcSf",
-    "Content-Type" : "application/json"
+    "x-api-key": ""
 }
-MERCHANT_ACCOUNT = "AdyenRecruitmentCOM"
+MERCHANT_ACCOUNT = ""
